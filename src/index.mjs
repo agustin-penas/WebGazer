be4186c2fb3a9e5e9f9248a6a0e24b29aa7285e6
Francisco Figari <figafran@gmail.com>
2022-11-07 10:12:43 -0300

    Remove https requirement

--- src/index.mjs
+++ src/index.mjs
@@ -648,10 +648,6 @@ function setUserMediaVariable(){
  * @returns {*}
  */
 webgazer.begin = function(onFail, initOptions) {
-  if (window.location.protocol !== 'https:' && window.location.hostname !== 'localhost' && window.chrome){
-    alert("WebGazer works only over https. If you are doing local development, you need to run a local server.");
-  }
-
   // Load model data stored in localforage.
   if (window.saveDataAcrossSessions) {
     loadGlobalData();
